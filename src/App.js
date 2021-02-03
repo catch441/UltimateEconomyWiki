76c2c58925212d8d2f3cd161bae06876613fb3da
Jonas Vargas <42014524+msallure@users.noreply.github.com>
2021-02-03 13:23:23 -0500

    added modules and updated code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,63 @@ import Header from './components/header.js';
 import HeaderNav from './components/headerNav.js';
 import Footer from './components/footer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <HeaderNav />
-      <Footer />
-    </div>
+    <Router>
+      <Switch>
+          <Route path="/job-system">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+          <Route path="/shop-system">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+          <Route path="/town-system">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+          <Route path="/player-features">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+          <Route path="/about">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+          <Route path="/download">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+          <Route path="/plugin-info">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+          <Route path="/change-log">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+          <Route path="/">
+            <Header />
+            <HeaderNav />
+            <Footer />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,13 @@
 import '../css/main.css';
 import '../css/footer.css';
+import { SiDiscord } from "react-icons/si";
+import { SiGithub } from "react-icons/si";
 
 function Footer() {
   return (
     <div id="footer-container">
       <p class="footer-text"><b>Wiki Dev:</b> Vera Vargas<br /><b>Wiki Last Updated:</b> February 27, 2021</p>
+      <p class="footer-icon"><SiDiscord /> <SiGithub /></p>
     </div>
   );
 }
